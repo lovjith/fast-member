6b15cf1659246db27e863d4f656bd2192e969c5b
Lovjith K T <lovjith@gmail.com>
2019-09-15 18:16:10 +0400

    Read the data from Db

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
 
 namespace ConsoleApp
 {
@@ -6,7 +9,26 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var watch = new Stopwatch();
+
+            var sdns = new List<Sdn>();
+            var sdn = new Sdn();
+
+            watch.Start();
+            using (var con = new SqlConnection("Data Source=10.10.10.200;Initial Catalog=Ofac;User ID=sa;Password=Password11"))
+            {
+                var cmd = new SqlCommand("SELECT * FROM Sdn;", con);
+                con.Open();
+                var reader = cmd.ExecuteReader(behavior: System.Data.CommandBehavior.CloseConnection);
+
+                while (reader.Read())
+                {
+                    sdn = reader.ConvertToObject<Sdn>();
+                    sdns.Add(sdn);
+                }
+            }
+            watch.Stop();
+            Console.WriteLine($"Time elapesed: {watch.ElapsedMilliseconds}ms for {sdns.Count} records");
         }
 
     }
---
+++ ConsoleApp/Sdn.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    public class Sdn
+    {
+        public int Id { get; set; }
+        public string _id { get; set; }
+        public byte SourceAgencyId { get; set; }
+        public byte SourceListTypeId { get; set; }
+        public string FirstName { get; set; }
+        public string Name2 { get; set; }
+        public string Name3 { get; set; }
+        public string Name4 { get; set; }
+        public string Name5 { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset RecordStateDate { get; set; }
+    }
+}
